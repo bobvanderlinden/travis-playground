ec668a659097b91e89706c463effff16f741a8eb
Bob van der Linden <bobvanderlinden@gmail.com>
2015-11-20 23:15:39 +0100

    build machinekit using bobvanderlinden/machinekit-build-debian-armhf

--- Makefile
+++
@@ -1,6 +0,0 @@
-main: main.c
-	gcc -o main main.c
-
-test: main
-	grep -R . /proc/sys/fs/binfmt_misc
-	./main
--- main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Test is succesful! Yay\n");
-    return 0;
-}
